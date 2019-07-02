e36f21f02457afb09c8fcde50ebf5c6ccb6899ef
Kodziak <kodziak1416@gmail.com>
2019-07-02 23:47:10 +0200

    Add axios get func.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    Login: {{ login }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      login: null
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8080/profile", {
+        headers: {
+          Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdG5hbWUiOiJQcnplbWVrIiwibGFzdG5hbWUiOiJ0ZXN0IiwidXNlcm5hbWUiOiJ0ZXN0MSJ9.q2GqayEj7j42kiHdbikNRyoDhQZWiX89uweiVuN4Bn8`
+        }
+      })
+      .then(response => (this.login = response.data.username));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
