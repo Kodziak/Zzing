7969305f74fda96c8afae7b7e33be402cbe9ac4c
Kodziak <kodziak1416@gmail.com>
2019-07-03 22:00:20 +0200

    Implement register handler.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,7 +2,7 @@
   <div>
     <h4>Register</h4>
     <form>
-      <label for="name">Name</label>
+      <label for="name">Username</label>
       <div>
         <input id="name" type="text" v-model="name" required autofocus />
       </div>
@@ -22,14 +22,6 @@
         <input id="password-confirm" type="password" v-model="password_confirmation" required />
       </div>
 
-      <label for="password-confirm">Is this an administrator account?</label>
-      <div>
-        <select v-model="is_admin">
-          <option value="1">Yes</option>
-          <option value="0">No</option>
-        </select>
-      </div>
-
       <div>
         <button type="submit" @click="handleSubmit">Register</button>
       </div>
@@ -38,6 +30,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props: ["nextUrl"],
   data() {
@@ -45,9 +39,29 @@ export default {
       name: "",
       email: "",
       password: "",
-      password_confirmation: "",
-      is_admin: null
+      password_confirmation: ""
     };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+
+      if (this.password > 1) {
+        axios
+          .post("http://localhost:3000/register", {
+            username: this.name,
+            email: this.email,
+            password: this.password,
+            firstname: "test_site"
+          })
+          .then(function(response) {
+            console.log(response);
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    }
   }
 };
 </script>
