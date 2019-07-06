341c2ec592fde294df63feaf5ff8b3119ccf5796
Kodziak <kodziak1416@gmail.com>
2019-07-07 00:20:35 +0200

    Add

--- src/components/UserBoard.vue
+++ src/components/UserBoard.vue
@@ -3,6 +3,7 @@
     <h1>Welcome to regular users page</h1>
     <h2>{{ msg }}</h2>
     <h2>Inser savings amount</h2>
+    <button id="logout" type="submit" @click="handleLogout">Logout</button>
   </div>
 </template>
 
@@ -13,6 +14,28 @@ export default {
       msg: "The commoners",
       saving: ""
     };
+  },
+  mounted() {
+    let token = localStorage.getItem("jwt");
+    this.$http
+      .get("http://localhost:3000/profile", {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then(response => {
+        this.msg = response.data.username;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  methods: {
+    handleLogout(e) {
+      e.preventDefault();
+      localStorage.removeItem("jwt");
+      this.$router.push("/");
+    }
   }
 };
 </script>
